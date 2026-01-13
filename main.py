e5fd3d7e09217d826c7e9c566ad5b8a8c9201b55
winnieotieno770 <winnieotieno770@gmail.com>
2026-01-13 16:33:32 +0300

    add Order class price imutability and validation logic

--- main.py
+++ main.py
@@ -41,6 +41,21 @@ class Order():
         self.customer = customer
         self.coffee = coffee
         self.price =  price 
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        if hasattr(self, "_price"):
+            raise AttributeError("Order price cannot be changed after it is set")
+        if isinstance(value, float) and 1.0 <= value <= 10.0:
+           self._price = value
+        else:
+            raise ValueError("Price must be a float between 1.0 and 10.0")
+        
+    
     def __repr__(self):
         return f"Order for {self.customer.name} :{self.coffee.name}"
 
